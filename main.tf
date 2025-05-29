040db47b4b49595105fa244bed34e268d1deb40f
LeonardoComar <leofcomar@gmail.com>
2025-05-28 22:50:00 -0300

    Ajuste no main.tf

--- main.tf
+++ main.tf
@@ -61,9 +61,9 @@ resource "aws_ecr_repository" "app_repo" {
         rulePriority = 1
         description  = "Keep last 5 images"
         selection = {
-          tagStatus     = "any"
-          countType     = "imageCountMoreThan"
-          countNumber   = 5
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 5
         }
         action = {
           type = "expire"
