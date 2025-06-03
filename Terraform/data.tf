193a72419d39f6847c8a74cf382ecefa8912ba53
LeonardoComar <leofcomar@gmail.com>
2025-06-03 19:09:33 -0300

    Alterações finais do EKS

--- Terraform/data.tf
+++ Terraform/data.tf
@@ -79,13 +79,4 @@ data "aws_security_group" "fastfood_security_group_rds" {
   }
 }
 
-data "aws_secretsmanager_secret" "aws_secretsmanager_secret_fastfood_4" {
-  name = "aws-secretsmanager-secret-fastfood-4"
-}
-
-data "aws_secretsmanager_secret_version" "aws_secretsmanager_secret_version_fastfood_4" {
-  secret_id = data.aws_secretsmanager_secret.aws_secretsmanager_secret_fastfood_4.id
-}
-
-
 
--- Terraform/module/kubernetes/kubernetes_secret.tf
+++ Terraform/module/kubernetes/kubernetes_secret.tf
@@ -2,8 +2,4 @@ resource "kubernetes_secret" "fastfoodapi_secrets" {
   metadata {
     name = var.secret_name
   }
-
-  data = {
-    postgres-connection-string      = var.postgres_connection_string
-  }
 }
